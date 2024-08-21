b7e4ffae19f7feef76744daceaeadc9896543496
ooni <gdbsrjsdn@gmail.com>
2024-08-21 21:32:11 +0900

    w6-Wed

---
+++ JavaDemo/src/work-spring/day0821_spring_mvc/src/main/java/com/grepp/controller/HelloController.java
@@ -0,0 +1,53 @@
+package com.grepp.controller;
+
+import com.grepp.model.ParamDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
+
+@Controller     // DispatcherServlet의 관리 대상 객체가 됨.
+public class HelloController {
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        // 여기서 컨트롤러가 하고자 하는 작업을 실행할거임.
+        System.out.println("is here??");
+        return "hi";    // 기본 설정이 forward임.
+    }
+
+    // 원래는 이런식으로 request 객체의 파라미터를 하나하나 처리해줬어야 함.
+//    @RequestMapping(value = "/bye", method = RequestMethod.GET)
+//    public ModelAndView bye(HttpServletRequest req) {
+//        System.out.println("param1 : " + req.getParameter("param1"));
+//        System.out.println("param2 : " + req.getParameter("param2"));
+//        System.out.println("param2 num : " + (Integer.parseInt(req.getParameter("param2")) + 100));
+//        return null;
+//    }
+
+    // 스프링이 @RequestParam 애노테이션을 통해 해당 파라미터 알아서 받아서 파싱해서 변수로 넣어줌.
+    // p1, p2는 지역변수라, 기본값 지정 안해놓으면 오류뜸.
+    @GetMapping("/bye")
+    public ModelAndView bye(@RequestParam("param1") String p1,
+                            @RequestParam(value = "param2", required = false, defaultValue = "1") int p2) {
+        System.out.println("param1 : " + p1);
+        System.out.println("param2 : " + (p2 + 100));
+        return null;
+    }
+
+    // 파라미터가 여러 개인데 한꺼번에 받아서 처리하고 싶을 때 객체 하나에 다 담아주기도 함 (DTO)
+    // 이때, 객체의 멤버변수 이름과 클라이언트 파라미터 이름을 일치는 시켜야 함.
+//    @GetMapping("/bye")
+//    public ModelAndView bye(ParamDTO param) {
+//        System.out.println(param);
+//        return null;
+//    }
+
+//    @GetMapping("/bye")
+//    public ModelAndView bye(@RequestParam Map<String, Object> param) {
+//        System.out.println(param);
+//        return null;
+//    }
+}
---
+++ JavaDemo/src/work-spring/day0821_spring_mvc/src/main/webapp/WEB-INF/views/view3.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: geonoo
+  Date: 2024. 8. 21.
+  Time: 오후 3:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+나도 age를 혹시 받아볼 수 있나?<br>
+<%=request.getParameter("age")%>
+</body>
+</html>
