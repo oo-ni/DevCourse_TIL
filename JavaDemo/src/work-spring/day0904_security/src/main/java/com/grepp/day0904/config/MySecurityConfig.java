28c05b0bf42e0a66e51f891279564b59313fb623
ooni <gdbsrjsdn@gmail.com>
2024-10-17 21:42:14 +0900

    w8-Wed

---
+++ JavaDemo/src/work-spring/day0904_security/src/main/java/com/grepp/day0904/config/MySecurityConfig.java
@@ -0,0 +1,64 @@
+package com.grepp.day0904.config;
+
+import com.grepp.day0904.util.GeonooFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration  // 예전 xml로 설정하던 항목들의 자바 버전
+@EnableWebSecurity  // DispatcherServlet보다 더 먼저 진행되어야 하는 설정이라 tomcat이 읽고 반영해줘야 함.
+public class MySecurityConfig {
+    @Autowired
+    public GeonooFilter geonooFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return
+                http.cors(AbstractHttpConfigurer::disable)   // cors 일단 비활성화. 추후 프론트와 연결할 때, 프론트 서버 cors만  풀고 나머지는 불허해야 함.
+                        .csrf(AbstractHttpConfigurer::disable)  // cookie 등을 활용하지 않고 있어서 csrf 비활성화. 추후 csrf 공격을 막고 싶으면 여기 활성화 하고 쿠키동의 설정 해줘야함.
+                        .httpBasic(AbstractHttpConfigurer::disable) // Baisc 인증 환경 아니고 jwt 토큰이니까 Basic 모드 비활성화
+                        .sessionManagement(AbstractHttpConfigurer::disable) // session 기반 아님.
+                        .authorizeHttpRequests(req -> {
+                            req.requestMatchers("/auth/**").permitAll();    // 로그인 하러 오는 애한테 왜 토큰없냐고 멱살잡으면 안되니까 들어오게 해주고.
+                            req.anyRequest().authenticated();   // 나머지는 다 인증돼야 들어오게 할거임!
+                        })
+//                        .addFilter(new GeonooFilter());   // Spring Bean에 등록해뒀기 때문에 new 쓰면 안됨!
+                        .addFilterAfter(geonooFilter, CorsFilter.class)
+                        .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
+
+// 시큐리티 6 이전 버전에서는 아래처럼.
+//public class MySecurityConfig extends WebSecurityConfigurerAdapter{
+//    @Override
+//    protected void configure(HttpSecurity http){
+//        http~~~~ 설정
+//        http.cors() // WebMvcConfig에서 이미 설정했으므로 기본 cors 설정.
+//        .and()
+//        .csrf()// csrf는 현재 사용하지 않으므로 disable
+//        .disable()
+//        .httpBasic()// token을 사용하므로 basic 인증 disable
+//        .disable()
+//        .sessionManagement()  // session 기반이 아님을 선언
+//        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        .and()
+//        .authorizeRequests() // /와 /auth/** 경로는 인증 안해도 됨.
+//        .antMatchers("/", "/auth/**").permitAll()
+//        .anyRequest() // /와 /auth/**이외의 모든 경로는 인증 해야됨.
+//        .authenticated();
+//    }
+//}
+
---
+++ JavaDemo/src/work-spring/day0904_security/src/main/java/com/grepp/day0904/util/GeonooFilter.java
@@ -0,0 +1,76 @@
+package com.grepp.day0904.util;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class GeonooFilter extends OncePerRequestFilter {    // request요청 한 번 당 처리해주는 필터. Filter는 DispatcherServlet보다 먼저 작업을 처리함.
+    @Autowired
+    private MyJwtTokenProvider myJwtTokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String token = parseBearerToken(request);
+            System.out.println("starat geonoo filter : " + token);      // 이런거 찍으면 안됨 원래...
+            if (token != null) {
+                String username = myJwtTokenProvider.validateUserToken(token);  // 유효하지 않은 토큰인 경우 throw exception
+                // 토큰이 유효하다면 인증이 완료된거임.
+                AbstractAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        username,   // 지금처럼 문자열 말고 다른 것도 들어갈 수 있는데, 예제에서는 UserDetails 라는 오브젝트를 넣는 경우가 많음. 우리는 안했음.
+                        null,
+                        AuthorityUtils.NO_AUTHORITIES   // 로그인이 된 상태에서도 관리자, 일반 사용자 등의 권한이 달라질 수 있음. 여기서 설정.
+                );
+                // 토큰에다가 기본적인 인증정보 외에 더 부가적으로 달아놓고 싶은 내용이 있으면 아래 Detail에서 더 담는 작업 수행 가능
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();       // 비어있는 컨텍스트 생성
+                securityContext.setAuthentication(authToken);       // 컨텍스트에 토큰 담고
+                SecurityContextHolder.setContext(securityContext);      // 홀더에 컨텍스트 고정
+            }
+        } catch (Exception ex) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);  // response는 기본 설정이  html 응답으로 되어있을 거라..
+
+            Map<String, Object> body = new HashMap<>(); // front한테 인증 불가에 대한 에러 메세지를 응답하고 싶어서
+            body.put("status", HttpServletResponse.SC_UNAUTHORIZED);
+            body.put("error", "Unauthorized~~~~~");
+            body.put("message", ex.getMessage());
+            body.put("path", request.getServletPath());
+
+            ObjectMapper mapper = new ObjectMapper(); // 평소에 @RequestBody, @ResponseBody 처리하면서 자바 <-> json 작업할 때 쓰이던 lib
+            mapper.writeValue(response.getOutputStream(), body); // 응답에 에러내용 json으로 만들어 보내기
+        }
+
+        filterChain.doFilter(request, response);    // 때에 따라서는 아래 예외 발생시에도 나머지 필터를 더 진행해야 할 수 있음.
+    }
+
+    // JWT 토큰을 활용하는 과정에서 토큰 형식이 아래처럼 진행되는 경우가 많음.
+    // Authorization : Bearer ~~~~~
+    // 저 Bearer라는 글자가 기능적인 역할은 없는데 토큰 개발하는 프론트 쪽에서 관습적으로 항상 붙이는거 같은...
+    private String parseBearerToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+
+        return null;
+    }
+}
